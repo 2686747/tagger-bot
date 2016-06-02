6f0ee01b713989d1ca4be14bac5c25f770dc0e97
2686747 <m.vakhnik.dev@gmail.com>
2016-06-03 00:39:07 +0300

    realized #23

--- src/main/java/org/tlg/bot/mem/msg/HelpMessage.java
+++ src/main/java/org/tlg/bot/mem/msg/HelpMessage.java
@@ -13,7 +13,9 @@ public class HelpMessage extends TextMessage {
     private static final String MSG = "Send to bot your pictures"
         + ", add tags to these media. After this you can "
         + "search these media by these tags "
-        + "and send it to your companion in a chat" ;
+        + "and send it to your companion in a chat.\n"
+        + "More details you can see here:\n"
+        + "http://2686747.github.io/tagger-bot/" ;
 
     public HelpMessage(final Long chatId) {
         super(chatId, HelpMessage.MSG);
